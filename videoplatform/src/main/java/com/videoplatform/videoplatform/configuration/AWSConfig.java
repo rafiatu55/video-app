b800fd4883c7cfa31629e97995f450f10d0a95b4
Rafiatu Ibrahim <irafiatu44@gmail.com>
2024-07-21 08:57:37 +0000

    Configured environment variables securely and removed sensitive information

--- videoplatform/src/main/java/com/videoplatform/videoplatform/configuration/AWSConfig.java
+++ videoplatform/src/main/java/com/videoplatform/videoplatform/configuration/AWSConfig.java
@@ -7,9 +7,13 @@ import software.amazon.awssdk.services.s3.S3Client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Configuration
 public class AWSConfig {
+
+    private static final Logger logger = LoggerFactory.getLogger(AWSConfig.class);
 
     @Value("${aws.accessKeyId}")
     private String accessKey;
@@ -22,6 +26,10 @@ public class AWSConfig {
 
     @Bean
     public S3Client s3Client() {
+        logger.info("AWS Access Key: {}", accessKey);
+        logger.info("AWS Secret Key: {}", secretKey);
+        logger.info("AWS Region: {}", region);
+
         return S3Client.builder()
                 .region(Region.of(region))
                 .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey)))
